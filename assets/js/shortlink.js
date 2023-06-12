841165418c30166a279aba4421bb82d2e8139114
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2023-06-12 23:03:14 +0700

    test

---
+++ assets/js/shortlink.js
@@ -0,0 +1,4 @@
+var app_url = &#39;//www.wts.pw/&#39;;
+var app_api_token = &#39;3307edfd3d9899837c132c11236830bd3df00b42&#39;;
+var app_advert = 2;
+var app_exclude_domains = [&quot;*.safelinkblog.com&quot;,&quot;google.com&quot;,&quot;github.com&quot;,&quot;*.facebook.com&quot;,&quot;trakteer.id&quot;,&quot;*.silveryasha.web.id&quot;,&quot;*.perpusindo.info&quot;,&quot;mangadex.org&quot;,&quot;fb.me&quot;,&quot;*.t.me&quot;,&quot;t.me&quot;,&quot;discord.gg&quot;];
